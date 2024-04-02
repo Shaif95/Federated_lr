1a15ead069afee988ee002e546610fbb421bc6c3
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2024-04-01 21:21:08 -0500

    A

--- flr_base/client.py
+++ flr_base/client.py
@@ -1,6 +1,6 @@
 import flwr as fl
 import tensorflow as tf
-
+import json
 # Load CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
@@ -9,6 +9,10 @@ model = tf.keras.applications.MobileNetV2(input_shape=(32, 32, 3), classes=10, w
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
 class CifarClient(fl.client.NumPyClient):
+    def __init__(self):
+        super().__init__()
+        self.acc_list = []  # Initialize acc_list within the class
+    
     def get_parameters(self, config):
         # Return model parameters as a list of NumPy ndarrays
         return model.get_weights()
@@ -16,7 +20,14 @@ class CifarClient(fl.client.NumPyClient):
     def fit(self, parameters, config):
         # Set model parameters, train model, return updated model parameters
         model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=2, batch_size=32, steps_per_epoch=3)
+        history = model.fit(x_train, y_train, epochs=5, batch_size=32, steps_per_epoch=3)
+        
+        # Append accuracy values to acc_list
+        self.acc_list.extend(history.history['accuracy'])
+
+        with open("client_accuracy.json", "w") as f:
+            json.dump(self.acc_list, f)
+        
         return model.get_weights(), len(x_train), {}
 
     def evaluate(self, parameters, config):
@@ -24,6 +35,7 @@ class CifarClient(fl.client.NumPyClient):
         model.set_weights(parameters)
         loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
         return loss, len(x_test), {"accuracy": float(accuracy)}
+
 
 # Start Flower client
 fl.client.start_client(server_address="127.0.0.1:8080", client=CifarClient().to_client())
